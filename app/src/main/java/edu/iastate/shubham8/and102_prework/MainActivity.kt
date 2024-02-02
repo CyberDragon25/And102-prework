91896f49b390f12a9f824dc23f914b604d595c83
Shubham <bhattacharya.shubham123@gmail.com>
2024-02-02 17:47:25 -0600

    Completed project

--- app/src/main/java/edu/iastate/shubham8/and102_prework/MainActivity.kt
+++ app/src/main/java/edu/iastate/shubham8/and102_prework/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.iastate.shubham8.and102_prework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.shashank.sony.fancytoastlib.FancyToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sayHiButton = findViewById<Button>(R.id.button2)
+        sayHiButton.setOnClickListener {
+           FancyToast.makeText(this, "Hi there! I am the cute doogo nice to meet you 🐶", FancyToast.LENGTH_LONG, FancyToast.INFO, false).show()
+        }
     }
 }
